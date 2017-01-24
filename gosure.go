98b02f63f585cce68a79d456cbe48f3e33e0101b
David Brown <davidb@davidb.org>
2017-01-23 23:07:49 -0700

    Implement scan command
    The scan command scans a tree and generates a new surefile representing
    its contents.  All of the paths are currently fixed.

--- gosure.go
+++ gosure.go
@@ -2,26 +2,70 @@ package main
 
 import (
 	"compress/gzip"
+	"fmt"
 	"log"
 	"os"
 
 	"davidb.org/code/gosure/sure"
+
+	"github.com/spf13/cobra"
 )
 
+var scanDir string
+
 func main() {
-	node, err := sure.ScanFs(".")
+	root := &cobra.Command{
+		Use:   "gosure command args ...",
+		Short: "File integrity management",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Usage()
+			log.Fatal("Invalid usage, TODO")
+		},
+	}
+
+	scan := &cobra.Command{
+		Use:   "scan",
+		Short: "Scan tree",
+		Long:  "Scan the tree and record integrity",
+		Run:   doScan,
+	}
+
+	root.AddCommand(scan)
+
+	pf := scan.PersistentFlags()
+	pf.StringVarP(&scanDir, "dir", "d", ".", "Directory to scan")
+
+	if err := root.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func doScan(cmd *cobra.Command, args []string) {
+	tree, err := sure.ScanFs(".")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	est := node.EstimateHashes()
+	est := tree.EstimateHashes()
 	prog := sure.NewProgress(est.Files, est.Bytes)
 	prog.Flush()
-	node.ComputeHashes(&prog)
+	tree.ComputeHashes(&prog)
 	prog.Flush()
 
+	writeSure(tree)
+	os.Rename("2sure.dat.gz", "2sure.bak.gz")
+	err = os.Rename("2sure.0.gz", "2sure.dat.gz")
+	if err != nil {
+		log.Printf("Unable to rename 2sure.0.gz: %v", err)
+	}
+}
+
+func writeSure(tree *sure.Tree) {
 	// Dump it out for now in gob format.
-	fd, err := os.Create("2sure.dat.gz")
+	// TODO: Choose a tmp name that is unique by incrementing the
+	// digit.
+	fd, err := os.Create("2sure.0.gz")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,7 +74,7 @@ func main() {
 	zfd := gzip.NewWriter(fd)
 	defer zfd.Close()
 
-	err = node.Encode(zfd)
+	err = tree.Encode(zfd)
 	if err != nil {
 		log.Fatal(err)
 	}
