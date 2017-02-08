471f49e37833f524ed0fc03a60fcff4e05ea55d7
David Brown <davidb@davidb.org>
2017-02-07 17:19:40 -0700

    store: Parser for surefile argument
    Add a parser that decodes a user-provided surefile argument, and
    attempts to determine what files to use.

---
+++ store/parse.go
@@ -0,0 +1,74 @@
+package store
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"strings"
+)
+
+// Parse attempts to determine the parameters of the Store structure
+// based on a user-specified path.  The path specified can be the path
+// to a directory.  In this case, we will look at possible filenames
+// to determine the other parameters.  The path can also give a
+// filename of one of the surefiles, and we will derive the name
+// information from that.
+//
+// If the error result is nil, the Store will have the parameters set
+// according to user preferences.  Otherwise, the error will give
+// details, and the Store will remain unchanged.
+func (s *Store) Parse(name string) error {
+	if fi, err := os.Stat(name); err == nil && fi.IsDir() {
+		// The path given is a directory, use the defaults.
+		s.Path = name
+		s.Base = ""
+		s.Plain = false
+		return nil
+	}
+
+	// Try splitting off the last filename from the path, and see
+	// if we find that directory.
+	dir := path.Dir(name)
+	if fi, err := os.Stat(dir); err != nil || !fi.IsDir() {
+		return NotDir(name)
+	}
+
+	plain := false
+	base := path.Base(name)
+
+	if strings.HasSuffix(base, ".gz") {
+		base = base[:len(base)-3]
+	} else {
+		plain = true
+	}
+
+	// Strip off the known suffixes.
+	ext := path.Ext(base)
+	if ext == ".dat" || ext == ".bak" {
+		base = base[:len(base)-4]
+	} else if ext != "" {
+		return InvalidName(name)
+	}
+
+	s.Path = dir
+	s.Base = base
+	s.Plain = plain
+
+	return nil
+}
+
+// NotDir is an error returned when the name doesn't describe an
+// existing path.
+type NotDir string
+
+func (n NotDir) Error() string {
+	return fmt.Sprintf("Path %q is not in an existant directory", string(n))
+}
+
+// InvalidName is an error returned if the name contains an unknown
+// extension.
+type InvalidName string
+
+func (n InvalidName) Error() string {
+	return fmt.Sprintf("Path %q has an unknown extension", string(n))
+}
---
+++ store/parse_test.go
@@ -0,0 +1,58 @@
+package store_test
+
+import (
+	"testing"
+
+	"davidb.org/code/gosure/store"
+)
+
+var parseTests = []struct {
+	name  string
+	err   error
+	store store.Store
+}{
+	{"/invalid/path", store.NotDir("/invalid/path"), store.Store{
+		"", "", false,
+	}},
+	{"/usr/bin", nil, store.Store{
+		"/usr/bin", "", false,
+	}},
+	{"/usr/bin/2sure.dat.gz", nil, store.Store{
+		"/usr/bin", "2sure", false,
+	}},
+	{"/usr/bin/2sure.bak.gz", nil, store.Store{
+		"/usr/bin", "2sure", false,
+	}},
+	{"/usr/bin/2sure.dat", nil, store.Store{
+		"/usr/bin", "2sure", true,
+	}},
+	{"/usr/bin/2sure.bak", nil, store.Store{
+		"/usr/bin", "2sure", true,
+	}},
+	{"/usr/bin/fred.dat.gz", nil, store.Store{
+		"/usr/bin", "fred", false,
+	}},
+	{"/usr/bin/fred.dat", nil, store.Store{
+		"/usr/bin", "fred", true,
+	}},
+	{"/usr/bin/fred", nil, store.Store{
+		"/usr/bin", "fred", true,
+	}},
+	{"/usr/bin/bogus.ext", store.InvalidName("/usr/bin/bogus.ext"), store.Store{
+		"", "", false,
+	}},
+}
+
+func TestParse(t *testing.T) {
+	for _, pt := range parseTests {
+		var st store.Store
+		err := st.Parse(pt.name)
+		if err != pt.err {
+			t.Fatalf("Unexpected error, got %v, expect %v", err, pt.err)
+		}
+
+		if st != pt.store {
+			t.Fatalf("Store mismatch, got %+v, expect %+v", st, pt.store)
+		}
+	}
+}
