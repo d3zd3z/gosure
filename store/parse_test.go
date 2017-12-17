abd3765444f29512c5cc87189e2967eda1b8ed53
David Brown <david.brown@linaro.org>
2017-12-16 19:43:51 -0700

    store: Fix the parse tester

--- store/parse_test.go
+++ store/parse_test.go
@@ -12,35 +12,42 @@ var parseTests = []struct {
 	store store.Store
 }{
 	{"/invalid/path", store.NotDir("/invalid/path"), store.Store{
-		"", "", false,
+		"", "", false, simpleTags(),
 	}},
 	{"/usr/bin", nil, store.Store{
-		"/usr/bin", "", false,
+		"/usr/bin", "", false, simpleTags(),
 	}},
 	{"/usr/bin/2sure.dat.gz", nil, store.Store{
-		"/usr/bin", "2sure", false,
+		"/usr/bin", "2sure", false, simpleTags(),
 	}},
 	{"/usr/bin/2sure.bak.gz", nil, store.Store{
-		"/usr/bin", "2sure", false,
+		"/usr/bin", "2sure", false, simpleTags(),
 	}},
 	{"/usr/bin/2sure.dat", nil, store.Store{
-		"/usr/bin", "2sure", true,
+		"/usr/bin", "2sure", true, simpleTags(),
 	}},
 	{"/usr/bin/2sure.bak", nil, store.Store{
-		"/usr/bin", "2sure", true,
+		"/usr/bin", "2sure", true, simpleTags(),
 	}},
 	{"/usr/bin/fred.dat.gz", nil, store.Store{
-		"/usr/bin", "fred", false,
+		"/usr/bin", "fred", false, simpleTags(),
 	}},
 	{"/usr/bin/fred.dat", nil, store.Store{
-		"/usr/bin", "fred", true,
+		"/usr/bin", "fred", true, simpleTags(),
 	}},
 	{"/usr/bin/fred", nil, store.Store{
-		"/usr/bin", "fred", false,
+		"/usr/bin", "fred", false, simpleTags(),
 	}},
 	{"/usr/bin/bogus.ext", store.InvalidName("/usr/bin/bogus.ext"), store.Store{
-		"", "", false,
+		"", "", false, simpleTags(),
 	}},
+}
+
+// Construct a simple tag with just a name.
+func simpleTags() map[string]string {
+	result := make(map[string]string)
+	result["name"] = "test"
+	return result
 }
 
 func TestParse(t *testing.T) {
@@ -51,7 +58,7 @@ func TestParse(t *testing.T) {
 			t.Fatalf("Unexpected error, got %v, expect %v", err, pt.err)
 		}
 
-		if st != pt.store {
+		if st.Path != pt.store.Path || st.Base != pt.store.Base || st.Plain != pt.store.Plain {
 			t.Fatalf("Store mismatch, got %+v, expect %+v", st, pt.store)
 		}
 	}
