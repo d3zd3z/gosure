6e39aed363874576386216e7a71f5345e22c829f
David Brown <david.brown@linaro.org>
2017-12-17 00:12:51 -0700

    Initial naming convention code
    This is mostly copied from the Rust code.

---
+++ weave/naming.go
@@ -0,0 +1,79 @@
+package weave
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// A NamingConvention determines the names of various temp files.  The
+// SCCS conventions are not followed, because they are not safe (this
+// code will never write to a file that already exists).
+type NamingConvention interface {
+	// Create a temporary file for writing.  Upon success, returns
+	// the full path of the file, and the opened file for writing.
+	// The path will refer to a new file that did not exist before
+	// this call.  On error, `err` will be set to an error.
+	TempFile() (string, *os.File, error)
+
+	// Return the pathname of the primary file.
+	MainFile() string
+
+	// Return the pathname of the backup file.
+	BackupFile() string
+
+	// Return if compression is requested on the main file.
+	IsCompressed() bool
+}
+
+// The SimpleNaming is a NamingConvention that has a basename, with
+// the main file having a specified extension, the backup file having
+// a ".bak" extension, and the temp files using a numbered extension
+// starting with ".0".  If the names are intended to be compressed, a
+// ".gz" suffix can also be added.
+type SimpleNaming struct {
+	Path       string // The directory for the files to be written.
+	Base       string // The base of the filename.
+	Ext        string // The extension to use for the main name.
+	Compressed bool   // Are these names to indicate compression.
+}
+
+func (sn *SimpleNaming) MakeName(ext string) string {
+	gz := ""
+	if sn.Compressed {
+		gz = ".gz"
+	}
+	return fmt.Sprintf("%s/%s.%s%s", sn.Path, sn.Base, ext, gz)
+}
+
+func (sn *SimpleNaming) MainFile() string {
+	return sn.MakeName(sn.Ext)
+}
+
+func (sn *SimpleNaming) BackupFile() string {
+	return sn.MakeName("bak")
+}
+
+func (sn *SimpleNaming) TempFile() (name string, file *os.File, err error) {
+	n := 0
+	for {
+		name := sn.MakeName(strconv.Itoa(n))
+
+		file, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0644)
+		if err == nil {
+			return name, file, nil
+		}
+
+		// Only continue if the error we get is because the
+		// file already exists.  Any other error is returned.
+		if !os.IsExist(err) {
+			return "", nil, err
+		}
+
+		n += 1
+	}
+}
+
+func (sn *SimpleNaming) IsCompressed() bool {
+	return sn.Compressed
+}
---
+++ weave/naming_test.go
@@ -0,0 +1,54 @@
+package weave_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"davidb.org/x/gosure/weave"
+)
+
+// Test that we can create basic temp names.
+func TestTemps(t *testing.T) {
+	tdir, err := ioutil.TempDir("", "weave-test-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(tdir)
+
+	nam := weave.SimpleNaming{
+		Path:       tdir,
+		Base:       "hahafile",
+		Ext:        "weave",
+		Compressed: true,
+	}
+
+	// Make sure we can create a bunch.
+	for i := 0; i < 10; i++ {
+		name1, file1, err := nam.TempFile()
+		if err != nil {
+			t.Fatal(err)
+		}
+		file1.Close()
+		expect := fmt.Sprintf("%s/hahafile.%d.gz", tdir, i)
+		if name1 != expect {
+			t.Fatalf("Expecting name %q, got %q", expect, name1)
+		}
+	}
+}
+
+// Test that temp fails properly if the directory is invalid.
+func TestInvalidName(t *testing.T) {
+	nam := weave.SimpleNaming{
+		Path:       "/invalid/path/to/file",
+		Base:       "stuff",
+		Ext:        "weave",
+		Compressed: true,
+	}
+
+	_, _, err := nam.TempFile()
+	if err == nil {
+		t.Fatal("Should not have been able to create temp file")
+	}
+}
