8c4c848c84a9526160450cd2222aa9d0cb1d5ef7
David Brown <david.brown@linaro.org>
2017-12-31 16:56:13 -0700

    Use multiple goroutines for hash update
    Use up to NumCPU workers to compute hashes, taking advantage of
    available CPUs.

--- sure/hashes.go
+++ sure/hashes.go
@@ -3,6 +3,8 @@ package sure
 import (
 	"log"
 	"path"
+	"runtime"
+	"sync"
 
 	"davidb.org/x/gosure/sha"
 )
@@ -38,25 +40,60 @@ func (e *Estimate) update(t *Tree) {
 
 // Update all of the file nodes that don't have hashes.
 func (t *Tree) ComputeHashes(prog *Progress, dir string) {
-	t.hashWalk(prog, dir)
+	cpus := runtime.NumCPU()
+
+	req := make(chan hashUpdate, 2*cpus)
+	var wg sync.WaitGroup
+
+	wg.Add(cpus)
+
+	for i := 0; i < cpus; i++ {
+		go updateWorker(req, &wg, prog)
+	}
+
+	t.hashWalk(prog, dir, req)
+	close(req)
+
+	// Wait for everyone to finish.
+	wg.Wait()
 }
 
-func (t *Tree) hashWalk(prog *Progress, name string) {
+// This message indicates a single path to compute a hash for.  The
+// result will be added to the given attributes.
+type hashUpdate struct {
+	path string
+	atts *RegAtts
+}
+
+func (t *Tree) hashWalk(prog *Progress, name string, req chan<- hashUpdate) {
 	for _, f := range t.Files {
 		atts, ok := f.Atts.(*RegAtts)
 		if ok && atts.Sha1 == nil {
-			hash, err := sha.HashFile(path.Join(name, f.Name))
-			if err != nil {
-				log.Printf("Unable to hash file: %v", err)
-				continue
+			req <- hashUpdate{
+				path: path.Join(name, f.Name),
+				atts: atts,
 			}
-			atts.Sha1 = hash
-			prog.Update(1, uint64(atts.Size))
 		}
 	}
 
 	// And the children.
 	for _, c := range t.Children {
-		c.hashWalk(prog, path.Join(name, c.Name))
+		c.hashWalk(prog, path.Join(name, c.Name), req)
 	}
+}
+
+// updateWorker pulls messages from 'req', hashes the file, and then
+// tells the wg when it is done.
+func updateWorker(req <-chan hashUpdate, wg *sync.WaitGroup, prog *Progress) {
+	for hu := range req {
+		hash, err := sha.HashFile(hu.path)
+		if err != nil {
+			log.Printf("Unable to hash file: %s", err)
+			continue
+		}
+		hu.atts.Sha1 = hash
+		prog.Update(1, uint64(hu.atts.Size))
+	}
+
+	wg.Done()
 }
--- sure/progress.go
+++ sure/progress.go
@@ -3,6 +3,7 @@ package sure
 import (
 	"fmt"
 	"io"
+	"sync"
 )
 
 // A progress meter.  Records files visited, and number of bytes
@@ -15,6 +16,8 @@ type Progress struct {
 
 	curBytes   uint64
 	totalBytes uint64
+
+	lock sync.Mutex
 }
 
 func NewProgress(files, bytes uint64, wr io.Writer) Progress {
@@ -28,13 +31,23 @@ func NewProgress(files, bytes uint64, wr io.Writer) Progress {
 // Update the progress meter, displaying it if enough time has
 // elapsed.
 func (p *Progress) Update(files, bytes uint64) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
 	p.curFiles += files
 	p.curBytes += bytes
-	p.Flush()
+	p.flush()
 }
 
 // Flush the output, regardless of any update needed.
 func (p *Progress) Flush() {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	p.flush()
+}
+
+// flush does the actual flush, assuming the lock is already taken.
+func (p *Progress) flush() {
 	fmt.Fprintf(p.wr, "%7d/%7d (%5.1f%%) files, %s/%s (%5.1f%%) bytes\n",
 		p.curFiles, p.totalFiles,
 		float64(p.curFiles)*100.0/float64(p.totalFiles),
